c379474596b33533e480e1fd23428c6586c219f4
Allyson Kyle <ak579422@ohio.edu>
2024-09-04 17:35:13 -0400

    Done

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Allyson Kyle
+ *        Date: 9/04/2024
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    /*add code*/
+    return 0;
+}// main
